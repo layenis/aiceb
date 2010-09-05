79ecdd189c03f32a977bc6f126d78b7b0d3f27e7
Fabricio Souza <compifab@gmail.com>
2010-09-05 02:47:20 -0300

    comitando

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+	# projeto de teste
+	# versao 1
+	# Fabricio Souza
+	
+	$a = 100;
+	$b = 200;
+	
+	if($a > $b) {
+		echo 'a maior que b';
+	}elseif($a < $b){
+		echo 'a menor que b';
+	}else{
+		echo 'a igual a b';
+	}
+?>
